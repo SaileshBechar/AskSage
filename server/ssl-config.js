ac8bc36f34fb3c733df09bfcb70559475f385787
Dinesh Condur <dcondur@wawanesa.com>
2018-10-19 10:56:37 -0400

    Reverting to http

--- server/ssl-config.js
+++
@@ -1,4 +0,0 @@
-var path = require('path'),
-fs = require("fs");
-exports.privateKey = fs.readFileSync(path.join(__dirname, '../deploy/privatekey.pem')).toString();
-exports.certificate = fs.readFileSync(path.join(__dirname, '../deploy/certificate.pem')).toString();

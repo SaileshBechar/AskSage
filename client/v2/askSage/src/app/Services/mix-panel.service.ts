58eccaae4c879fb353756614d0800deb58b9764e
Dinesh Condur <dcondur@wawanesa.com>
2018-11-15 08:26:54 -0500

    Fixing mixpanel names

--- client/v2/askSage/src/app/Services/mix-panel.service.ts
+++ client/v2/askSage/src/app/Services/mix-panel.service.ts
@@ -7,28 +7,31 @@ import * as mixpanel from 'mixpanel-browser';
 export class MixPanelService {
   constructor() { }
 
-   /**
-   * Initialize mixpanel.
-   *
-   * @param {string} userId
-   * @memberof MixpanelService
-   */
+  /**
+  * Initialize mixpanel.
+  *
+  * @param {string} userId
+  * @memberof MixpanelService
+  */
   init(userId: string): void {
     mixpanel.init('fa268afdf77e936a3280b21b1f55391a');
     // mixpanel.track("Success");
     mixpanel.identify(userId);
   }
+  
+  setUser(obj: any): void {
+    mixpanel.people.set(obj);
+  }
 
 
-
-    /**
-   * Push new action to mixpanel.
-   *
-   * @param {string} id Name of the action to track.
-   * @param {*} [action={}] Actions object with custom properties.
-   * @memberof MixpanelService
-   */
-  track(id: string, action: any = {}): void {
+  /**
+ * Push new action to mixpanel.
+ *
+ * @param {string} id Name of the action to track.
+ * @param {*} [action={}] Actions object with custom properties.
+ * @memberof MixpanelService
+ */
+  track(id: string, action: any): void {
     mixpanel.track(id, action);
   }
 
@@ -37,5 +40,5 @@ export class MixPanelService {
 
 
 
- 
+
 }
